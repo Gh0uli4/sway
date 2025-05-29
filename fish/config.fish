2e25caffedee96924e66adfe1c6a30149b00b403
Gh0uli4 <zxcGh0uli4@yandex.ru>
2025-05-29 20:00:19 +0300

    Описание изменений

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,6 @@
 neofetch
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+function fish_greeting
 end
 if test -z "$DISPLAY" && test (tty) = "/dev/tty1"
     exec sway
 end
-function fish_greeting
-end
-
-set -gx QT_QPA_PLATFORM xcb
-set -gx ELECTRON_OZONE_PLATFORM_HINT x11
-
-set -Ux QT_QPA_PLATFORMTHEME qt5ct
-set -Ux QT_QPA_PLATFORMTHEME qt6ct
-
